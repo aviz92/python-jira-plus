cc1af576d378bbb0a431dcb183fa3b5b2050d158
Avi Zaguri <49598021+aviz92@users.noreply.github.com>
2025-06-10 15:12:36 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-package_version = '1.0.9'
+package_version = '1.0.10'
 
 package_name = 'python-jira-plus'
 package_description = 'Enhanced Python client for JIRA with better error handling, pagination, and metadata validation.'
